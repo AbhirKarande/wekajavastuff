a5fca5603731b59a3d82417793f88c741a67a661
AbhirHI <abhir511@gmail.com>
2022-03-23 16:49:52 -0400

    added newer data and made the option to choose whether or not to do sequential feature selection

--- project2.java
+++ project2.java
@@ -16,6 +16,8 @@ public class project2 {
 			String window_size = input.next();
 			System.out.println("Algo (1:J48 2:RF 3:SMO):");
 			int algo = input.nextInt();
+			System.out.println("Sequential feature selection?(y/n):");
+			boolean seq = input.next().equals("y");
 			
 			/* Run the python script that generate features.csv */
 			try {
@@ -46,78 +48,80 @@ public class project2 {
 				double max = 0;
 				int index = 0;
 				
-				// choose the most important feature
-				for(int i = 0; i<features.length; i++) {
-					String arffData = MyWekaUtils.csvToArff(csvData, new int[]{i});
-					double accuracy = MyWekaUtils.classify(arffData, algo);
-					//System.out.println(accuracy);
-					if(accuracy > max) {
-						max = accuracy;
-						index = i;
-					}
-				}
-				featureList.add(index);
-				double current_accuracy = max; // accuracy so far
-				System.out.println("most_important_feature : "+index);
-				System.out.println("current_accuracy: "+current_accuracy);
-				System.out.println();
-				
-				// iteratively choose the most important feature from the rest until there's not much gain in accuracy
-				int temp = 1;
-				while(temp < features.length) {
-					max = 0; // current max accuracy
-					index = 0; // index of best
-					
-					// try all possible combinations
-					for(int idx = 0; idx < features.length; idx++) {
-						if(featureList.indexOf(idx) < 0) {
-							featureList.add(idx);
-							
-							// train and get accuracy
-							String arffData = MyWekaUtils.csvToArff(csvData, convertIntegers(featureList));
-							double accuracy = MyWekaUtils.classify(arffData, algo);
-							//System.out.println(featureList.toString());
-							
-							// compare accuracy
-							if(accuracy > max) {
-								max = accuracy;
-								index = idx;
-							}
-							featureList.remove(temp);
+				if(seq) {
+					// choose the most important feature
+					for(int i = 0; i<features.length; i++) {
+						String arffData = MyWekaUtils.csvToArff(csvData, new int[]{i});
+						double accuracy = MyWekaUtils.classify(arffData, algo);
+						//System.out.println(accuracy);
+						if(accuracy > max) {
+							max = accuracy;
+							index = i;
 						}
 					}
+					featureList.add(index);
+					double current_accuracy = max; // accuracy so far
+					System.out.println("most_important_feature : "+index);
+					System.out.println("current_accuracy: "+current_accuracy);
+					System.out.println();
 					
-					// check accuracy gain
-					if(max - current_accuracy >= 1) {
-						featureList.add(index);
-						current_accuracy = max;
-						System.out.println("new_feature: "+index);
-						System.out.println("current_accuracy: "+current_accuracy);
-						System.out.println();
-					}else {
-						if(max - current_accuracy > 0) {
+					// iteratively choose the most important feature from the rest until there's not much gain in accuracy
+					int temp = 1;
+					while(temp < features.length) {
+						max = 0; // current max accuracy
+						index = 0; // index of best
+						
+						// try all possible combinations
+						for(int idx = 0; idx < features.length; idx++) {
+							if(featureList.indexOf(idx) < 0) {
+								featureList.add(idx);
+								
+								// train and get accuracy
+								String arffData = MyWekaUtils.csvToArff(csvData, convertIntegers(featureList));
+								double accuracy = MyWekaUtils.classify(arffData, algo);
+								//System.out.println(featureList.toString());
+								
+								// compare accuracy
+								if(accuracy > max) {
+									max = accuracy;
+									index = idx;
+								}
+								featureList.remove(temp);
+							}
+						}
+						
+						// check accuracy gain
+						if(max - current_accuracy >= 1) {
 							featureList.add(index);
 							current_accuracy = max;
 							System.out.println("new_feature: "+index);
+							System.out.println("current_accuracy: "+current_accuracy);
+							System.out.println();
+						}else {
+							if(max - current_accuracy > 0) {
+								featureList.add(index);
+								current_accuracy = max;
+								System.out.println("new_feature: "+index);
+							}
+							System.out.println("gain in accuracy is less than 1%.");
+							System.out.print("features selected: ");
+							System.out.println(featureList.toString());
+							System.out.println("accuracy: "+current_accuracy);
+							System.out.println();
+							break;
 						}
-						System.out.println("gain in accuracy is less than 1%.");
-						System.out.print("features selected: ");
-						System.out.println(featureList.toString());
-						System.out.println("accuracy: "+current_accuracy);
-						System.out.println();
-						break;
+						
+						temp += 1;		
 					}
-					
-					temp += 1;		
+				}else {
+					/* transform csv to arff */
+					String arffData = MyWekaUtils.csvToArff(csvData, features);
+		          	// System.out.println(arffData);
+		            
+		          	// train the model
+		          	double accuracy = MyWekaUtils.classify(arffData, algo);
+		          	System.out.println(accuracy);
 				}
-				
-//				/* transform csv to arff */
-//				String arffData = MyWekaUtils.csvToArff(csvData, features);
-//	          	// System.out.println(arffData);
-//	            
-//	          	// train the model
-//	          	double accuracy = MyWekaUtils.classify(arffData, algo);
-//	          	System.out.println(accuracy);
 	            
 	        } catch (Exception ex) {
 	            System.out.println(ex.toString());
